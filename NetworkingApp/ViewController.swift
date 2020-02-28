8b0e7c17b24ad1c94a0193b42169f16415136cab
InnaL <innalapteva72@gmail.com>
2020-02-28 07:50:59 +0500

    Initial Commit

---
+++ NetworkingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkingApp
+//
+//  Created by Manager on 28/02/2020.
+//  Copyright © 2020 Inna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
